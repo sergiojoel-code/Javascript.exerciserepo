0012d784a7f05fad52b0d8af2e4c8a197f535901
sergiosalazar <sjoelsalazar92@gmail.com>
2025-04-19 01:28:31 -0300

    primeros ejercicios con operadores principales

---
+++ OPERADORES Y ESTRUCTURAS DE CONTROL/operadores.js
@@ -0,0 +1,18 @@
+// OPERADORES
+
+console.log(`------------------Asignación------------------`);
+let num = 10;
+console.log(`num: ${num}\n`);
+
+// Asignación suma
+console.log(`num: ${num} || num += 2 => ${(num += 2)}`);
+// Asignación resta
+console.log(`num: ${num} || num -= 2 => ${(num -= 2)}`);
+// Asignación multiplicación
+console.log(`num: ${num} || num *= 2 => ${(num *= 2)}`);
+// Asignación exponencial
+console.log(`num: ${num} || num **= 2 => ${(num **= 2)}`);
+// Asignación división
+console.log(`num: ${num} || num /= 2 => ${(num /= 2)}`);
+// Asignación resto
+console.log(`num: ${num} || num %= 2 => ${(num %= 2)}`);
