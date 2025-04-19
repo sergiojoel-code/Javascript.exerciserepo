d2b8564f033995e82f1d623621794f7584db7dfe
sergiosalazar <sjoelsalazar92@gmail.com>
2025-04-19 17:43:45 -0300

    Ejemplo de ejercicios con operadores de comparación

--- OPERADORES Y ESTRUCTURAS DE CONTROL/operadores.js
+++ OPERADORES Y ESTRUCTURAS DE CONTROL/operadores.js
@@ -16,3 +16,21 @@ console.log(`num: ${num} || num **= 2 => ${(num **= 2)}`);
 console.log(`num: ${num} || num /= 2 => ${(num /= 2)}`);
 // Asignación resto
 console.log(`num: ${num} || num %= 2 => ${(num %= 2)}`);
+
+console.log(`-----------------Comparación------------------`);
+let num1 = 20;
+let num2 = 27;
+console.log(`num1: ${num1}     num2: ${num2}\n`);
+
+// Igual
+console.log(`num1 == num2? => ${num1 == num2}`);
+console.log(`'27' == num2? => ${'27' == num2} | Convierte tipos, comparación débil`);
+// No igual
+console.log(`num1 != num2? => ${num1 != num2}`);
+console.log(`'27' != num2? =>${"27" != num2} | Convierte tipos, comparación débil`);
+// Estricta
+console.log(`num1 === num2? => ${num1 === num2}`);
+console.log(`'27' === num2? => ${"27" === num2} | No convierte tipos, comparación fuerte`);
+// No igual estricto
+console.log(`num1 !== num2? => ${num1 !== num2}`);
+console.log(`'27' !== num2? => ${"27" !== num2} | No convierte tipos, comparación fuerte`);
