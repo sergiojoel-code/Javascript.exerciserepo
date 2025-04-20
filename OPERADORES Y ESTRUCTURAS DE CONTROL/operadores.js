9cc1a01522819bf27cbe1f8ce9bb6a24c26c1a86
sergiosalazar <sjoelsalazar92@gmail.com>
2025-04-20 20:12:04 -0300

    ejemplo de operadores aritméticos

--- OPERADORES Y ESTRUCTURAS DE CONTROL/operadores.js
+++ OPERADORES Y ESTRUCTURAS DE CONTROL/operadores.js
@@ -1,6 +1,6 @@
 // OPERADORES
 
-console.log(`------------------Asignación------------------`);
+console.log(`----------------- Asignación -----------------`);
 let num = 10;
 console.log(`num: ${num}\n`);
 
@@ -17,20 +17,53 @@ console.log(`num: ${num} || num /= 2 => ${(num /= 2)}`);
 // Asignación resto
 console.log(`num: ${num} || num %= 2 => ${(num %= 2)}`);
 
-console.log(`-----------------Comparación------------------`);
+console.log(`---------------- Comparación -----------------`);
 let num1 = 20;
 let num2 = 27;
 console.log(`num1: ${num1}     num2: ${num2}\n`);
 
 // Igual
 console.log(`num1 == num2? => ${num1 == num2}`);
-console.log(`'27' == num2? => ${'27' == num2} | Convierte tipos, comparación débil`);
+console.log(
+  `'27' == num2? => ${"27" == num2} | Convierte tipos, comparación débil`
+);
 // No igual
 console.log(`num1 != num2? => ${num1 != num2}`);
-console.log(`'27' != num2? =>${"27" != num2} | Convierte tipos, comparación débil`);
+console.log(
+  `'27' != num2? =>${"27" != num2} | Convierte tipos, comparación débil`
+);
 // Estricta
 console.log(`num1 === num2? => ${num1 === num2}`);
-console.log(`'27' === num2? => ${"27" === num2} | No convierte tipos, comparación fuerte`);
+console.log(
+  `'27' === num2? => ${"27" === num2} | No convierte tipos, comparación fuerte`
+);
 // No igual estricto
 console.log(`num1 !== num2? => ${num1 !== num2}`);
-console.log(`'27' !== num2? => ${"27" !== num2} | No convierte tipos, comparación fuerte`);
+console.log(
+  `'27' !== num2? => ${"27" !== num2} | No convierte tipos, comparación fuerte`
+);
+
+// Mayor que
+console.log(`num1 > num2? => ${num1 > num2}`);
+// Mayor o igual que
+console.log(`num1 >= num2? => ${num1 >= num2}`);
+// Menor que
+console.log(`num1 < num2? => ${num1 < num2}`);
+// Menor o igual que
+console.log(`num1 <= num2? => ${num1 <= num2}`);
+
+console.log(`---------------- Aritméticos -----------------`);
+// Adicionales a las operaciones +, -, *, **, / y %, Javascript también tiene las siguientes operaciones:
+let x = 2;
+let y;
+console.log(`x: ${x}    y: ${y}\n`);
+
+// Incrementar
+console.log(`++x => x = ${x}; y = ${++x}`);
+// Decrementar
+console.log(`--x => x = ${x}; y = ${--x}`);
+// Unary negation => Regresa la negación del operando
+console.log(`x => -x || x = ${x} => -x = ${-x}`);
+// Unary plus => Intenta convertir el operando en número
+console.log(`+x => x || x = ${-x} => +x = ${+x}`);
+console.log(`+'x' => x || x = '27' => +x = ${+"27"}\n`);
