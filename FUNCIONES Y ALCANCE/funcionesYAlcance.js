d39cd09ccfc8661b3c158209cf34fb9fc81e9450
sergiosalazar <sjoelsalazar92@gmail.com>
2025-04-23 10:45:08 -0300

    Carpeta con ejemplo de Funciones y alcance de las mismas

---
+++ FUNCIONES Y ALCANCE/funcionesYAlcance.js
@@ -0,0 +1,8 @@
+/* Crea ejemplos de funciones básicas que representen las diferentes posibilidades del lenguaje:
+ Sin parámetros ni retorno, con uno o varios parámetros, con retorno... */
+
+//  Funciones declaradas (Function declarations)
+function saludar() {
+    console.log("Hola!");
+}
+saludar();
